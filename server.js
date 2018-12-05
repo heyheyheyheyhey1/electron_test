6593a0a9fca52d701a88cfbf55161667e9334570
heyheyheyheyhey1 <912126472@qq.com>
2018-12-05 19:17:15 +0800

    file trans support

--- server.js
+++ server.js
@@ -1,16 +1,46 @@
 const express = require('express')
 const app = express()
+const fs = require('fs')
+const muilter = require('multer')({
+    dest: "FileRecv/"
+})
 const ws = require('ws')
 const http = require('http')
 const httpServer = http.createServer(app)
-const wsSever=new ws.Server({server:httpServer})
+const wsSever = new ws.Server({ server: httpServer })
+
+//express部分
+app.use(express.static("./"))
+app.use(muilter.any())
+app.use("/",(req,res,next)=>{
+    console.log("incomming request")
+    next()
+})
 
 
-wsSever.on("connection",(connection)=>{
+app.post("/uploadFiles", (req, res) => {
+    let file = req.files[0]
+    console.log(file)
+    let name =  file.filename
+    let originalname=file.originalname
+    if (originalname.split(".").length>1){
+        fs.rename(`FileRecv/${name}`,`FileRecv/${name}.${originalname.split(".")[originalname.split(".").length-1]}`)
+        
+    }
+    res.send("nice")
+   
+})
+
+
+
+
+
+//ws 部分
+wsSever.on("connection", (connection) => {
     console.log('new connection')
-    connection.on('message',(msg)=>{
+    connection.on('message', (msg) => {
         console.log(msg)
-        wsSever.clients.forEach((el)=>{
+        wsSever.clients.forEach((el) => {
             el.send(msg)
         })
     })
@@ -24,7 +54,7 @@ wsSever.on("connection",(connection)=>{
     // }
 })
 
-wsSever.on("error",(err)=>{
+wsSever.on("error", (err) => {
     console.log(err)
 })
 
